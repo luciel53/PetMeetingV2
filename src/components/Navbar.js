662ae88ab0f62316a9c46ff5cd47ffe1e540b734
luciel53 <l.leroty@gmail.com>
2024-03-03 21:06:44 +0100

    useState on icons connection/unconnection

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,26 @@ import { Link } from 'react-router-dom';
 import userIcon from "../assets/images/icons/user.svg";
 import messagesIcon from "../assets/images/icons/messages.svg";
 import logoutIcon from "../assets/images/icons/logout.svg";
+import registerIcon from "../assets/images/icons/register.png";
+import connectionIcon from "../assets/images/icons/connection.png";
 
 export default function Navbar() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [connectedIcons, setConnectedIcons] = useState(false);
+	const [unconnectedIcons, setUnconnectedIcons] = useState(false);
 
 	function toggleMenu () {
 	  setIsMenuOpen(!isMenuOpen);
+	  if (setIsMenuOpen(true)) {
+		setUnconnectedIcons(false);
+	  } else {
+		setUnconnectedIcons(true);
+	  }
+	}
+
+
+	function toggleUnconnectedIcons () {
+      setUnconnectedIcons(!unconnectedIcons);
 	}
 
 	return (
@@ -68,9 +82,9 @@ export default function Navbar() {
 		  </ul>
 		  {/* Parallelogram */}
 		  <div className="bg-gray w-28 md:w-36 lg:w-60 h-12 md:h-14 lg:h-20 skew-x-45 mr-8 md:mr-10 lg:mr-48 flex flex-row px-4 md:px-6 lg:px-12 justify-between items-center">
-			<img src={userIcon} className="-skew-x-45 w-5 md:w-6 lg:w-8" alt="Icône de profil utilisateur" />
-			<img src={messagesIcon} className="-skew-x-45 w-5 md:w-6 lg:w-8" alt="Icône de profil utilisateur" />
-			<img src={logoutIcon} className="-skew-x-45 w-5 md:w-6 lg:w-8" alt="Icône de profil utilisateur" />
+			{connectedIcons ? <img src={userIcon} className="-skew-x-45 w-5 md:w-6 lg:w-8" alt="Icône de profil utilisateur" /> : <img src={connectionIcon} className="-skew-x-45 w-9 md:w-6 lg:w-8 mr-2" alt="Connexion" /> }
+			{connectedIcons ? <img src={messagesIcon} className="-skew-x-45 w-5 md:w-6 lg:w-8" alt="Icône de profil utilisateur" /> : <img src={registerIcon} className="-skew-x-45 w-10 md:w-6 lg:w-8" alt="Enregistrement" />}
+			{connectedIcons && <img src={logoutIcon} className="-skew-x-45 w-5 md:w-6 lg:w-8" alt="Icône de profil utilisateur" /> }
 		  </div>
 		  {/* Burger button */}
 		  <button
