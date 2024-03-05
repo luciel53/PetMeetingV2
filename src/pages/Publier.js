4f6c156f34153509bb3af55d3f0b8d3503e2ee1c
luciel53 <l.leroty@gmail.com>
2024-03-06 00:09:05 +0100

    update publier page

--- src/pages/Publier.js
+++ src/pages/Publier.js
@@ -2,10 +2,10 @@ import add from '../assets/images/icons/add.png';
 
 export default function Publier() {
   return (
-    <div className='container flex flex-col'>
-      <div className='flex flex-row'>
-        <img src={add} alt="ajouter une annonce" width="20" height="20" />
-        <h2 className="mx-auto mt-28 text-purple text-2xl">
+    <div className='container flex flex-col mx-auto'>
+      <div className='container flex flex-row mx-auto mb-6'>
+        <img src={add} className='mt-28 z-2 w-6 h-6 ml-auto' alt="ajouter une annonce" />
+        <h2 className="pl-3 mt-28 mr-auto text-purple text-2xl">
           Ajoutez une annonce:
         </h2>
       </div>
