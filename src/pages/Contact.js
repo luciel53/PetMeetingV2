670bcae2819011a044513e08cd119e16350e1dc4
luciel53 <l.leroty@gmail.com>
2024-03-07 00:05:16 +0100

    update contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,23 +3,31 @@ import Button from "../components/Button";
 
 export default function Contact () {
 	return (
-		<div className="container flex flex-col justify-center mx-auto md:mx-auto w-[88%] md:w-[55%] lg:w-[25%] p-5 md:p-12 my-28 md:mt-72 bg-white rounded-3xl shadow-lg text-sm md:text-lg">
-      <h2>Connectez-vous:</h2>
-      <label className="ml-4 mb-2">Nom d'utilisateur*:</label>
+		<div className="container flex items-center flex-col justify-center mx-auto md:mx-auto w-[88%] md:w-[55%] lg:w-[25%] p-5 md:p-12 my-28 md:mt-44 bg-white rounded-3xl shadow-xl text-sm md:text-lg">
+      <h2 className="text-lg md:text-2xl text-fragole mx-auto mb-6">
+        Contactez-nous:
+      </h2>
+      <label className="ml-4 mb-2">Nom:</label>
+      <input
+        type="email"
+        className="h-8 w-[90%] ml-4 mb-4 pt-1 pl-3 border border-darkgray rounded-lg bg-gray"
+      />
+      <label className="ml-4 mb-2">Email:</label>
       <input
         type="text"
         className="h-8 w-[90%] ml-4 mb-4 pt-1 pl-3 border border-darkgray rounded-lg bg-gray"
       />
-      <label className="ml-4 mb-2">Mot de passe*:</label>
+      <label className="ml-4 mb-2">Sujet:</label>
       <input
         type="text"
         className="h-8 w-[90%] ml-4 mb-4 pt-1 pl-3 border border-darkgray rounded-lg bg-gray"
       />
-      <Button text="Connexion" />
-      <hr class="h-px my-8 bg-gray-200 border-0 bg-darkgray"></hr>
-      <p className="mx-auto">
-        Pas de compte? <em className="text-fragole">Enregistrez-vous</em>
-      </p>
+      <label className="ml-4 mb-2">Votre message:</label>
+      <input
+        type="text"
+        className="h-24 w-[90%] ml-4 mb-4 pt-1 pl-3 border border-darkgray rounded-lg bg-gray"
+      />
+      <Button text="Envoyer" />
     </div>
 	)
 }
