15b949b65ca16ddd278a7abb77901c0809a64b03
luciel53 <l.leroty@gmail.com>
2024-03-06 23:03:15 +0100

    update the grid of annonces' page

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,21 @@
+import cats from "./Cats";
+
 export default function Grid() {
+  console.log(cats);
+
   return (
-    <div className="grid grid-cols-2 place-content-center md:max-w-max md:mx-auto md:grid-cols-3 lg:grid-cols-5">
-      <div class="vignette flex justify-center items-center text-center"></div>
+    <div className="grid grid-cols-2 place-content-center md:max-w-max md:mx-auto md:grid-cols-3 lg:grid-cols-4 mt-12">
+      {cats.map((cat, index) =>
+      <div key={index} className="vignette flex flex-col justify-center items-center text-center bg-white z-0 md:h-96 w-72 mx-auto mr-3 mt-3 pb-2 rounded-3xl shadow-xl">
+        <p className="text-center text-lg font-semibold mt-3">{cat.name}</p>
+        <img
+          src={cat.image}
+          className="z-20 h-40 md:h-72 my-3 object-cover mx-auto"
+          alt={cat.name}
+        />
+        <p className="text-center font-semibold mb-1">{cat.race}</p>
+      </div>
+      )}
     </div>
   );
 }
